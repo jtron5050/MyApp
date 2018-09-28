9fd60b5f7ca24403063d13f1d64bdbf1d4da2acb
Josh Thomas <jathomas@commandalkon.com>
2018-09-28 11:46:49 -0400

    change hello to derp

--- src/MyAppConsole/Program.cs
+++ src/MyAppConsole/Program.cs
@@ -6,7 +6,7 @@ namespace MyAppConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello derp!");
         }
     }
 }
